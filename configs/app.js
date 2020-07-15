b1f2a811c087df2b2be50eb00bde72a445743971
Chainarong <moe.cute@live.com>
2020-07-15 11:31:09 +0700

    add environment

--- configs/app.js
+++ configs/app.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
+const path = require('path')
+const processEnvironment = require('dotenv').config({ path: path.resolve(__dirname, `../${process.env.NODE_ENV}.env`) })
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: processEnvironment.parsed.PORT || 3000,
   isProduction: process.env.NODE_ENV === 'production',
-  apiVersion: process.env.API_VERSION || 1,
-  token_exp_days: process.env.TOKEN_EXP_DAYS || 1,
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'my-secret',
-  mongodbUri: process.env.MONGODB_URI,
-  pageLimit: process.env.PAGE_LIMIT || 15
+  apiVersion: processEnvironment.parsed.API_VERSION || 1,
+  token_exp_days: processEnvironment.parsed.TOKEN_EXP_DAYS || 1,
+  secret: processEnvironment.parsed.NODE_ENV === 'production' ? process.env.SECRET : 'my-secret',
+  mongodbUri: processEnvironment.parsed.MONGODB_URI,
+  pageLimit: processEnvironment.parsed.PAGE_LIMIT || 15
 }
